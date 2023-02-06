95016b86307a71ad931108cff4a0b0d4701e0b5a
haraldsvik <haraldsvik91@gmail.com>
2023-02-06 21:20:48 +0100

    Completed minimum solution in python

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 
+import time
+
+
 def read_file(file_name):
+    names = []
     with open(file_name, "r") as f:
-        return f.read()
+        for line in f:
+            names.append(line.strip())
+    return names
+
+
+def remove_anagrams_with_one_name(anagrams):
+    return [anagram for anagram in anagrams if len(anagram) > 1]
 
 
 def find_anagrams(names):
-  obj = {}
+    obj = {}
 
-  for name in names:
-    sorted_name = ''.join(sorted(name))
+    for name in names:
+        sorted_name = ''.join(sorted(name.lower()))
 
-    if sorted_name in obj:  
-      obj[sorted_name].append(name)
-    else:
-      obj[sorted_name] = [name]
+        if sorted_name in obj:
+            if name not in obj[sorted_name]:
+                obj[sorted_name].append(name)
+        else:
+            obj[sorted_name] = [name]
 
-  return obj.values()
- 
+    return list(obj.values())
+
 
 def run():
     population = read_file("population.txt")
+
+    before = time.time()
     anagrams = find_anagrams(population)
-    print(anagrams)
+    no_duplicates = remove_anagrams_with_one_name(anagrams)
+    after = time.time()
+
+    print(no_duplicates)
+    print("Time: ", (after - before) * 1000, "ms")
 
 
 if __name__ == "__main__":
--- main.test.py
+++ main.test.py
@@ -1,31 +1,63 @@
+import time
 import unittest
 
-from main import find_anagrams
-
+from main import find_anagrams, read_file, remove_anagrams_with_one_name
 
 
 class OppgaveTest(unittest.TestCase):
     def setUp(self):
-    #self.population = [ "erik", "kire", "lene"]
-        self.population = []
-        with open("population.txt", "r") as f:
-            for line in f:
-                self.population.append(line.strip())
-            
+        self.population = read_file("population.txt")
 
-    def test_input(self):
+    def test_input_is_not_none(self):
         self.assertIsNotNone(self.population)
 
     def test_population_is_list(self):
         self.assertIsInstance(self.population, list)
-    
+
     def test_population_is_not_empty(self):
         self.assertTrue(len(self.population) > 0)
 
     def test_find_anagrams(self):
         anagrams = find_anagrams(self.population)
+
         self.assertIsInstance(anagrams, list)
-        self.assertTrue(len(anagrams) > 0)    
+        self.assertTrue(len(anagrams) > 0)
+
+    def test_find_case_insensitive_anagrams(self):
+        args = ["erik", "Kire", "lene"]
+        anagrams = find_anagrams(args)
+        self.assertEqual(len(anagrams), 2)
+
+    def test_retuns_empty_list_if_no_anagrams(self):
+        args = [""]
+        anagrams = find_anagrams(args)
+        self.assertEqual(len(anagrams), 1)
+
+    def test_no_duplicate_anagrams(self):
+        anagrams = find_anagrams(self.population)
+        for anagram in anagrams:
+            self.assertEqual(len(anagram), len(set(anagram)))
+
+    def test_no_empty_anagrams(self):
+        anagrams = find_anagrams(self.population)
+        no_duplicates = remove_anagrams_with_one_name(anagrams)
+
+        for anagram in no_duplicates:
+            self.assertTrue(len(anagram) > 1)
+
+    def test_check_if_to_slow(self):
+        before = time.time()
+        anagrams = find_anagrams(self.population)
+        no_duplicates = remove_anagrams_with_one_name(anagrams)
+        after = time.time()
+
+        time_taken = (after - before) * 1000
+        name_lenght = len(self.population)
+        time_per_name = time_taken / name_lenght
+
+        # 0.0015 ms per name is an arbitrary limit
+        self.assertTrue(time_per_name < 0.0015)
+
 
 if __name__ == '__main__':
     unittest.main()
